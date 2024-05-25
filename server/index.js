37535602f70f3a39708f25960764b2b21cc23713
jihunp <edwardpark.park@gmail.com>
2024-05-24 22:54:13 -0700

    created backend template

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-// const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 
-app.use("/", (req, res) => {
-  res.send("server is running");
-});
+const PEXEL_KEY = process.env.PEXEL_API_KEY;
+
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`server is running ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
