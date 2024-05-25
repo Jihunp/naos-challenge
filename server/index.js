ace7562e82c656e45287cafe613ad289a8145a93
jihunp <edwardpark.park@gmail.com>
2024-05-25 00:11:53 -0700

    api for backend is functional

--- server/index.js
+++ server/index.js
@@ -11,6 +11,25 @@ app.use(cors());
 const PEXEL_KEY = process.env.PEXEL_API_KEY;
 
 
+app.get("/pexels/photos", async (req, res) => {
+  try {
+    const query = "tiger";
+    const response = await axios.get("https://api.pexels.com/v1/search", {
+      params: {
+        query,
+        per_page: 7
+      },
+      headers: {
+        Authorization: PEXEL_KEY
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error occurred while fetching photos from Pexels:", error);
+    res.status(500).json({ error: "An error occurred while fetching photos from Pexels" });
+  }
+});
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
